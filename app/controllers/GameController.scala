dca826a1123d07b854d7ac008db98c79af8569ff
Schmidt-jan <ja163sch@htwg-konstanz.de>
2022-10-05 13:52:37 +0200

    play via query

---
+++ app/controllers/GameController.scala
@@ -0,0 +1,108 @@
+package controllers
+
+import Schach.GameFieldModule
+import Schach.aview.Tui
+import Schach.controller.controllerComponent.ControllerInterface
+import Schach.model.figureComponent.Figure
+import com.google.inject.{Guice, Injector}
+import play.api.libs.json.{JsObject, Json}
+import play.api.mvc._
+
+import javax.inject.Inject
+
+
+class GameController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  val injector: Injector = Guice.createInjector(new GameFieldModule)
+  val controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
+  val tui = new Tui(controller)
+  controller.notifyObservers
+  var response: Vector[Figure] = Vector.empty
+
+  def tuiGame(cmd: String): Action[AnyContent] = Action {
+    if (cmd.nonEmpty)
+      tui.interactWithUser(cmd)
+
+    val player = if (controller.getPlayer().getRed == 0) "BLACK" else "WHITE"
+    val status = printGameStatus()
+    val gameField = controller.gameFieldToString
+
+    val response = "It's your turn : " + player + "\n" +
+      "Status: " + status + "\n" +
+      gameField
+    Ok(response)
+  }
+
+  def printGameStatus(): String = {
+    controller.getGameStatus() match {
+      case 0 => "RUNNING"
+      case 1 => "PLAYER " + {
+          if (controller.getPlayer().getRed == 0) "Black"
+          else "WHITE"
+        } + "IS CHECKED"
+      case 2 => {if (controller.getPlayer().getRed == 0) "BLACK "
+      else "WHITE "} + "IS CHECKMATE"
+      case 3 => "INVALID MOVE"
+      case 4 => "PAWN HAS REACHED THE END"
+    }
+  }
+
+  def figureMove(from: String, to: String): Action[AnyContent] = Action {
+    if (from.length != 2)
+      BadRequest("Length of 'from' needs to be 2")
+    if (to.length != 2)
+      BadRequest("Length of 'from' needs to be 2")
+
+    val fromX = getPoint(from.charAt(0))
+    val fromY = getPoint(from.charAt(1))
+    val toX = getPoint(to.charAt(0))
+    val toY = getPoint(to.charAt(1))
+    val fromTo = Vector(fromX, fromY, toX, toY)
+
+    controller.movePiece(fromTo)
+    Ok(controller.getGameField.toString())
+  }
+
+  def figureSwitch: Action[AnyContent] = Action {
+    NotImplemented
+  }
+
+
+  def getPoint(input: Char): Int = {
+    input match {
+      case 'A' => 0
+      case 'B' => 1
+      case 'C' => 2
+      case 'D' => 3
+      case 'E' => 4
+      case 'F' => 5
+      case 'G' => 6
+      case 'H' => 7
+      case '1' => 0
+      case '2' => 1
+      case '3' => 2
+      case '4' => 3
+      case '5' => 4
+      case '6' => 5
+      case '7' => 6
+      case '8' => 7
+      case _ => -1
+    }
+  }
+
+  def gameFieldToJson: JsObject = {
+    val figures = controller.getGameField
+    val player = if (controller.getPlayer.getRed == 0) "BLACK" else "WHITE"
+
+    Json.obj(
+      "gameField" -> Json.arr(
+        figures.map { t =>
+          Json.obj(
+            "color" -> player,
+            "checked" -> t.checked,
+            "x" -> t.x,
+            "y" -> t.y)
+        }
+      )
+    )
+  }
+}
